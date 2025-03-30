4505917f04cedadfa9136949093bd583fa4892b5
ateBitBug <75680000+ateBitBug@users.noreply.github.com>
2025-03-30 08:20:46 -0400

    Add files via upload

---
+++ if-all-in-one-test-1-98-2.py
@@ -0,0 +1,733 @@
+class Parent:
+    def __init__(self):
+        print('class Parent: __init__')
+
+        self.parent1 = 1
+        self.parent2 = 2
+
+    def getParentWorld(self):
+        print('Parent: Earth')
+
+
+
+class Child(Parent):
+    def __init__(self):
+        super().__init__()
+        print('__init__: Child')
+
+        self.child1 = 1
+        self.child2 = 2
+
+    def getChildWorld(self):
+        print('Child: Ocean')
+
+    def getIndex(self):
+        return [0,1,2]
+
+'''
+
+'''
+
+child = Child()
+child.getChildWorld()
+child.getIndex()
+
+
+a = 1
+b = 2
+c = None
+
+
+
+# the intellisense fails when 45-46 is uncommented
+# if (a == 1 and b == 2):
+#     c = 3
+
+
+# worked 
+if (a == 1 and b == 2):
+    c = 3
+if (a == 1 and b == 2):
+    c = 3
+if (a == 1 and b == 2):
+    c = 3
+if (a == 1 and b == 2):
+    c = 3
+if (a == 1 and b == 2):
+    c = 3
+if (a == 1 and b == 2):
+    c = 3
+if (a == 1 and b == 2):
+    c = 3
+if (a == 1 and b == 2):
+    c = 3
+if (a == 1 and b == 2):
+    c = 3
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+if (a == 1 and b == 2):
+    c = 3
+if (a == 1 and b == 2):
+    c = 3
+if (a == 1 and b == 2):
+    c = 3
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+if (a == 1 and b == 2):
+    c = 3
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
+
+if (a == 1 and b == 2):
+    c = 3
