fbf44d2afebc23a3ffd9806fd544757e3d5b3d38
ateBitBug <75680000+ateBitBug@users.noreply.github.com>
2025-04-05 06:58:53 -0400

    Add files via upload

--- if-all-in-one-test-1-98-2.py
+++ if-all-in-one-test-1-98-2.py
@@ -1,3 +1,27 @@
+'''
+vscode: 1.98.2
+os: windows 11
+
+extensions:
+    pylance Version 2025.4.1
+        Identifier ms-python.vscode-pylance
+        Version 2025.4.1
+        Last Updated
+        2025-04-04, 05:21:39
+    
+    python Version 2025.2.0
+        Identifier ms-python.python
+        Version 2025.2.0
+        Last Updated
+        2025-03-30, 07:59:26
+    
+    python debugger
+        Identifier ms-python.debugpy
+        Version 2025.4.1
+        Last Updated
+        2025-03-30, 07:59:26
+'''
+
 class Parent:
     def __init__(self):
         print('class Parent: __init__')
@@ -38,7 +62,7 @@ c = None
 
 
 '''
-The intellisense fails when line# 55-56 is uncommented which is the 235th
+The intellisense fails when line# 77-78 is uncommented which is the 235th
 'if statement'.
 
 When the line are uncommented, the intellisense fails to provide
@@ -47,19 +71,18 @@ the auto complete feature for the CHILD object.
 For example, when typing 'child.', nothing shows up.
 
 SUMMARY:
-When line# 55-56 is uncommented:
-    vscode version 1.84.2 intellisense works without any issues.
-        - This version's intellisense works with unlimited 'if statements'.
-    vscode version 1.98.2 intellisense fails when line# are uncommented.
-        - in fact, the intellisense has been failing since Nov2023 1.85.0.
+When line# 77-78 is uncommented:
+    vscode version 1.98.2 intellisense fails when these line# are uncommented.
 '''
 # if (a == 1 and b == 2):
 #     c = 3
+
 
 '''
 Try 'child.' here or anywhere in the code.
 With the line# 55-56 uncommented, the intellisense fails.
 '''
+child.getChildWorld()
 
 
 '''
